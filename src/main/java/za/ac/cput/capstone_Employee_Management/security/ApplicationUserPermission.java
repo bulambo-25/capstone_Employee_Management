2d0a13b9f751d7817b4584e51e0ca90dd14a8302
martinez <reach>
2022-10-27 21:08:00 +0200

    security completed

--- src/main/java/za/ac/cput/capstone_Employee_Management/security/ApplicationUserPermission.java
+++ src/main/java/za/ac/cput/capstone_Employee_Management/security/ApplicationUserPermission.java
@@ -1,19 +1,19 @@
-package za.ac.cput.capstone_Employee_Management.security;
-
-public enum ApplicationUserPermission {
-        EMPLOYEE_SAVE("employee: create"),
-        EMPLOYEE_VIEW("employee: view"),
-     EMPLOYEE_EDIT("employee: edit"),
-    EMPLOYEE_DELETE("employee: delete");
-
-    private final String permission;
-
-
-    ApplicationUserPermission(String permission) {
-        this.permission = permission;
-    }
-
-    public String getPermission() {
-        return permission;
-    }
-}
+//package za.ac.cput.capstone_Employee_Management.security;
+//
+//public enum ApplicationUserPermission {
+//        EMPLOYEE_SAVE("employee: create"),
+//        EMPLOYEE_VIEW("employee: view"),
+//     EMPLOYEE_EDIT("employee: edit"),
+//    EMPLOYEE_DELETE("employee: delete");
+//
+//    private final String permission;
+//
+//
+//    ApplicationUserPermission(String permission) {
+//        this.permission = permission;
+//    }
+//
+//    public String getPermission() {
+//        return permission;
+//    }
+//}
